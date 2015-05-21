a0a9cd3b9560f4dc165f82828ba7807eba1c64aa
Brian Capouch <brianc@proto.palaver.net>
2015-05-21 01:45:46 -0400

    Fix erroneous commit message

--- ch1/ex1-11.js
+++
@@ -1,8 +0,0 @@
-function showScope() {
-  scope = "local";
-  return scope;
-}
-scope = "global";
-print(scope); // displays "global"
-print(showScope()); // displays "local"
-print(scope); // displays "local"
