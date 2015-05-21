27377c661653e28508ea5061e264c614623fa64b
Brian Capouch <brianc@proto.palaver.net>
2015-05-21 01:33:06 -0400

    A subprocedure or void function in JavaScript

---
+++ ch1/ex1-10.js
@@ -0,0 +1,8 @@
+function curve(arr, amount) {
+  for (var i = 0; i < arr.length; ++i) {
+    arr[i] += amount;
+  }
+}
+var grades = [77, 73, 74, 81, 90];
+curve(grades, 5);
+print(grades); // displays 82,78,79,86,95
