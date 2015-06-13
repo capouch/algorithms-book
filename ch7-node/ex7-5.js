1f5a0e48260822a90b7125bac8ed3db0c405bc68
Brian Capouch <brianc@proto.palaver.net>
2015-06-13 19:05:37 -0400

    A sorted dictionary display

---
+++ ch7-node/ex7-5.js
@@ -0,0 +1,11 @@
+var Dictionary = require('./Dictionary.js').Dictionary;
+
+var pbook = new Dictionary();
+pbook.add("Raymond","123");
+pbook.add("David", "345");
+pbook.add("Cynthia", "456");
+pbook.add("Mike", "723");
+pbook.add("Jennifer", "987");
+pbook.add("Danny", "012");
+pbook.add("Jonathan", "666");
+pbook.showAll();
