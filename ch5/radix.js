1a8aeda40d53f1b5835f615723972b0f87e8fb36
Brian Capouch <brianc@proto.palaver.net>
2015-06-13 16:11:19 -0400

    Functions used by radix sort

---
+++ ch5/radix.js
@@ -0,0 +1,26 @@
+function distribute(nums, queues, n, digit) {
+ for (var i = 0; i < n; ++i) {
+  if (digit == 1) {
+    queues[nums[i]%10].enqueue(nums[i]);
+    }
+  else {
+    queues[(Math.floor(nums[i]/10))].enqueue(nums[i]);
+    }
+  }
+}
+
+function collect(queues, nums) {
+ var i = 0;
+ for (var digit = 0; digit < 10; ++digit) {
+   while (!queues[digit].empty()) {
+     nums[i++] = queues[digit].dequeue();
+     }
+   }
+}
+
+function dispArray(arr) {
+ for (var i = 0; i < arr.length; ++i) {
+   putstr(arr[i] + " ");
+   }
+ }
+
