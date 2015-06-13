af9cdbba0835ad7d200980b7465dcea57c1f5120
Brian Capouch <brianc@proto.palaver.net>
2015-06-13 19:04:56 -0400

    Using the count() and clear() functions

---
+++ ch7-node/ex7-4.js
@@ -0,0 +1,11 @@
+var Dictionary = require('./Dictionary.js').Dictionary;
+
+var pbook = new Dictionary();
+pbook.add("Raymond","123");
+pbook.add("David", "345");
+pbook.add("Cynthia", "456");
+console.log("Number of entries: " + pbook.count());
+console.log("David's extension: " + pbook.find("David"));
+pbook.showAll();
+pbook.clear();
+console.log("Number of entries: " + pbook.count());
