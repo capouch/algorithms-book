b4e3ca35a1285fda8ed252fed0401abe42ccc87b
Brian Capouch <brianc@proto.palaver.net>
2015-06-19 02:59:10 -0400

    seqSearch() with better self-organization Ex. 13-10

---
+++ ch13/seqSearchBetter.js
@@ -0,0 +1,19 @@
+function seqSearch(arr, data) {
+  for (var i = 0; i < arr.length; ++i) {
+    if (arr[i] == data && i > (arr.length * 0.2)) {
+      swap(arr,i,0);
+      return true;
+    }
+    else if (arr[i] == data) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function swap(arr, index, index1) {
+  temp = arr[index];
+  arr[index] = arr[index1];
+  arr[index1] = temp;
+}
+
