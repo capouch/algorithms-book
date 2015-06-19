05d0079d217e294153ac6b5870682c8f7fe5cc96
Brian Capouch <brianc@proto.palaver.net>
2015-06-19 02:52:15 -0400

    The findMax() function Ex. 13-7

---
+++ ch13-node/findMax.js
@@ -0,0 +1,11 @@
+function findMax(arr) {
+  var max = arr[0];
+  for (var i = 1; i < arr.length; ++i) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+module.exports.findMax = findMax;
