182431dc9ed6145680493f08ba131c74841f188d
Brian Capouch <brianc@proto.palaver.net>
2015-06-20 01:56:27 -0400

    Iterative Fibonacci function

---
+++ ch14-node/iterFib.js
@@ -0,0 +1,13 @@
+function iterFib(n) {
+  var last = 1;
+  var nextLast = 1;
+  var result = 1;
+  for (var i = 2; i < n; ++i) {
+    result = last + nextLast;
+    nextLast = last;
+    last = result;
+  }
+  return result;
+}
+
+console.log(iterFib(10));
